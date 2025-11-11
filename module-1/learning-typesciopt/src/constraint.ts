e14fd7cf4a3e9f0fc267e0de069bac2e4ab5db62
sayed725 <ssayed72533@gmail.com>
2025-11-11 19:27:57 +0600

    learned more type of types

---
+++ module-1/learning-typesciopt/src/constraint.ts
@@ -0,0 +1,34 @@
+// constraint : strict rules deya
+
+type Student = { id: number; name: string; dateOfBirth: string; class: string };
+
+const addStudentToCourse1 = <T extends Student>(studentInfo: T) => {
+  return {
+    course: "Next Level",
+    ...studentInfo,
+  };
+};
+
+const studentt1 = {
+  id: 123,
+  name: "Mezba",
+  hasPen: true,
+};
+
+const studentt2 = {
+  id: 321,
+  name: "Jhankar Mahbub",
+  hasCar: true,
+  isMarried: true,
+};
+
+const studentt3 = {
+  id: 444,
+  name: "Abdur Rakib",
+  hasWatch: true,
+  dateOfBirth: "20-20-2020",
+  class: "1",
+};
+
+const constraintResult1 = addStudentToCourse1(studentt3);
+console.log(constraintResult1);
